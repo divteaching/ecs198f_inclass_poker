b07b3c05e16efc4643158d5d626d010cc35916a5
Divyansh Rajesh Jain <drajeshjain@ucdavis.edu>
2025-01-31 18:04:57 -0800

    01/31/2025 Lecture

---
+++ utils/deck.py
@@ -0,0 +1,42 @@
+#Standard Library Imports
+import random
+
+class Card:
+    def __init__(self, suite: "S" | 'C' | 'H' | 'D', face: int):
+        """
+        1 --> ACE
+        2 - 10 --> Number Cards
+        11 --> Jack
+        12 --> Queen
+        13 --> King
+        """
+        if face < 1 or face > 14:
+            raise Exception("Invalid face")
+
+        self.suite = suite
+        self.face = face
+
+    def __str__(self):
+        return f"{self.suite}:{self.face}"
+    
+class Deck:
+    def __init__(self):
+        self.create_cards()
+
+    def create_cards(self):
+        self.cards = []
+        suites = ["S", "C", "H", "D"]
+        faces = [i for i in range(1, 14, 1)]
+
+        for suite in suites:
+            for face in faces:
+                self.cards.append(Card(suite, face))
+
+    def shuffle(self):
+        #Shuffle the deck
+        random.shuffle(self.cards)
+
+    def draw_card(self):
+        if len(self.cards) <= 0:
+            raise Exception("Deck is empty")
+        return self.cards.pop(0)
